6a912efc96cc94b6cecfcfabfc87c8f346884d99
windows99-hue <3013907412@qq.com>
2026-07-12 21:59:06 +0800

    在avatar_test.py里测试可行性，估计还有未发现的bug

--- avatar_test.py
+++ avatar_test.py
@@ -20,13 +20,24 @@ sayori = dokibox.Avatar(name="Sayori", emotes={
 
     "shocked":[sayori_images[sayori_image_root + "1l.png"],
                sayori_images[sayori_image_root + "1r.png"],
-               sayori_images[sayori_image_root + "c.png"]]
+               sayori_images[sayori_image_root + "c.png"]],
+    
+    "suprised":[sayori_images[sayori_image_root + "2l.png"],
+               sayori_images[sayori_image_root + "2r.png"],
+               sayori_images[sayori_image_root + "m.png"]]
 })
 
 yuri = dokibox.Avatar(name="Yuri", emotes={
-    "normal":[yuri_images[yuri_image_root + "1l.png"],yuri_images[yuri_image_root + "1r.png"],yuri_images[yuri_image_root + "a.png"],]
+    "normal":[yuri_images[yuri_image_root + "1l.png"],
+              yuri_images[yuri_image_root + "1r.png"],
+              yuri_images[yuri_image_root + "a.png"]],
+
+    "smiled":[yuri_images[yuri_image_root + "1l.png"],
+              yuri_images[yuri_image_root + "1r.png"],
+              yuri_images[yuri_image_root + "c.png"]]
 })
 
 dokibox.dialogbox("你好呀！我是纱世里！",name=sayori,sprites=[sayori("left", "normal")])  
 dokibox.dialogbox("你好呀！我是优里！",name=yuri,sprites=[sayori("left", "normal"),yuri("right", "normal")])
-dokibox.dialogbox("优里!?你也在这里！",name=sayori,sprites=[sayori("left", "shocked"),yuri("right", "normal")])
+dokibox.dialogbox("优里!?你也在这里！",name=sayori,sprites=[sayori("left", "suprised"),yuri("right", "normal")])
+dokibox.dialogbox("我也不知道，听夏树说这里很好玩就来啦，看这里还有很多树木~",name=yuri,sprites=[sayori("left", "suprised"),yuri("right", "smiled")])
