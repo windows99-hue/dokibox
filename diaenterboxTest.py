b4da76361bb8015b08ad10557de767036c7f2572
windows99-hue <3013907412@qq.com>
2026-07-15 14:00:35 +0800

    diaenterbox完成

--- diaenterboxTest.py
+++ diaenterboxTest.py
@@ -4,7 +4,7 @@ from avatar_test import sayori
 cmd = dokibox.diaenterbox(name="MC",allow_empty=True)
 print(repr(cmd))
 
-dokibox.dialogbox("哇！MC你来啦！",name=sayori, sprites=[sayori("center", "happy")])
+dokibox.dialogbox("Wow! MC you're here!",name=sayori, sprites=[sayori("center", "happy")])
 # dokibox.dialogbox("12")
 cmd = dokibox.diaenterbox(name="MC",sprites=[sayori("center", "normal")],overflow_mode="overflow")
 print(repr(cmd))
