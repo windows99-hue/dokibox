fec226d2513fce242bd203d37c595d8bc8f71152
windows99-hue <3013907412@qq.com>
2026-07-17 13:30:14 +0800

    通过了和其他窗口匹配工作的测试

--- cmdboxTest.py
+++ cmdboxTest.py
@@ -1,20 +1,10 @@
 import dokibox
 import os
 
-# dokibox.cmdbox("print('hello world')", runcmd=True, language="python")
-# dokibox.closecmdbox(1000)
-# dokibox.cmdbox("print('goooooooood')",runcmd=True)
-# dokibox.dialogbox("Hello, world!",fdst=True)
-# dokibox.cmdbox("x = 1 + 2 + 3\nprint('result:', x)", runcmd=True, language="python")
-# dokibox.cmdbox("echo hello from cmd", runcmd=True, language="cmd",clear=True)
+dokibox.ynbox("确定吗？")
 
-# dokibox.dialogbox("看到了吗")
-# dokibox.closecmdbox()
-# dokibox.dialogbox("算算数真好玩嘿嘿")
-
-#dokibox.cmdbox("import os\nos.remove(\"E:\\\\99之没事写的小程序\\\\99ddlcmsgbox\\\\01-images\\\\cover4.png\")",runcmd=True)
 def del_cover():
-    path = r"E:\99之没事写的小程序\99ddlcmsgbox\01-images\cover4.png"
+    path = r"E:\99之没事写的小程序\99ddlcmsgbox\01-images\cover111.png"
     try:
         os.remove(path)
         return "cover4.png 已成功删除"
@@ -29,8 +19,10 @@ dokibox.cmdbox(
     result=del_cover
 )
 dokibox.cmdbox("os.remove(\"y_cg1.png\")",result="y_cg1.png 已成功删除。")
+dokibox.textbox("123")
 dokibox.cmdbox("os.remove(\"n_cg2.png\")",result="n_cg2.png 已成功删除。")
+dokibox.diaenterbox(name="MC",fdst=True)
 dokibox.cmdbox("os.remove(\"n_cg1.png\")",result="n_cg1.png 已成功删除。")
 dokibox.cmdbox("",result="")
 
-dokibox.dialogbox("")
+dokibox.dialogbox("好耶！")
