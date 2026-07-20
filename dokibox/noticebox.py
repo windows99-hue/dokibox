72009ebb7f342e3407951d54f6a3437747be5ed8
windows99-hue <3013907412@qq.com>
2026-07-20 10:18:07 +0800

    初步构建了notice

---
+++ dokibox/noticebox.py
@@ -0,0 +1,152 @@
+# -*- coding: utf-8 -*-
+"""dokibox.noticebox -- top-left notice/toast notification"""
+import sys
+import ctypes
+from PySide6.QtCore import Qt, QTimer, QPropertyAnimation, QEasingCurve, QEventLoop
+from PySide6.QtGui import (
+    QPainter, QColor, QFont, QFontMetrics, QLinearGradient, QBrush, QPen,
+)
+from PySide6.QtWidgets import QWidget, QGraphicsOpacityEffect
+from dokibox._base import _get_app, _get_dpi_scale
+
+NOTICE_HEIGHT = 28
+NOTICE_WIDTH_RATIO = 0.55
+FONT_SIZE = 12
+TEXT_COLOR = "#555555"
+FADE_DURATION = 300
+
+DWMWA_BORDER_COLOR = 34
+DWMWA_SHADOW_OPACITY = 33
+
+
+class MARGINS(ctypes.Structure):
+    _fields_ = [
+        ("cxLeftWidth",     ctypes.c_int),
+        ("cxRightWidth",    ctypes.c_int),
+        ("cxTopHeight",     ctypes.c_int),
+        ("cxBottomHeight",  ctypes.c_int),
+    ]
+
+
+def _remove_dwm_frame(hwnd):
+    margins = MARGINS(-1, -1, -1, -1)
+    ctypes.windll.dwmapi.DwmExtendFrameIntoClientArea(hwnd, ctypes.byref(margins))
+
+
+def _remove_window_shadow(hwnd):
+    zero_val = ctypes.c_uint(0)
+    ctypes.windll.dwmapi.DwmSetWindowAttribute(
+        hwnd, DWMWA_SHADOW_OPACITY,
+        ctypes.byref(zero_val), ctypes.sizeof(zero_val),
+    )
+
+
+class _NoticeWidget(QWidget):
+
+    def __init__(self, msg, last, x, y, w):
+        _get_app()
+        super().__init__(None)
+        self._msg = msg
+        dpi_s = 1.0 / _get_dpi_scale()
+        h = int(NOTICE_HEIGHT * dpi_s)
+
+        flags = Qt.FramelessWindowHint | Qt.Tool | Qt.WindowStaysOnTopHint
+        self.setWindowFlags(flags)
+        self.setAttribute(Qt.WA_TranslucentBackground)
+        self.setGeometry(x, y, w, h)
+        self.setFixedSize(w, h)
+
+        self._opacity = QGraphicsOpacityEffect(self)
+        self._opacity.setOpacity(1.0)
+        self.setGraphicsEffect(self._opacity)
+
+        self._close_timer = QTimer(self)
+        self._close_timer.setSingleShot(True)
+        self._close_timer.timeout.connect(self._fade_out)
+        self._close_timer.start(int(last * 1000))
+
+    def _fade_out(self):
+        self._anim = QPropertyAnimation(self._opacity, b"opacity")
+        self._anim.setDuration(FADE_DURATION)
+        self._anim.setStartValue(1.0)
+        self._anim.setEndValue(0.0)
+        self._anim.setEasingCurve(QEasingCurve.InQuad)
+        self._anim.finished.connect(self._close)
+        self._anim.start()
+
+    def _close(self):
+        self.hide()
+        self.deleteLater()
+
+    def showEvent(self, event):
+        super().showEvent(event)
+        if sys.platform != 'win32':
+            return
+        try:
+            hwnd = int(self.winId())
+            _remove_dwm_frame(hwnd)
+            _remove_window_shadow(hwnd)
+            ctypes.windll.dwmapi.DwmSetWindowAttribute(
+                hwnd, DWMWA_BORDER_COLOR,
+                ctypes.byref(ctypes.c_int(0xFFFFFFFE)),
+                ctypes.sizeof(ctypes.c_int),
+            )
+        except Exception:
+            pass
+
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        painter.setRenderHint(QPainter.Antialiasing)
+
+        w, h = self.width(), self.height()
+
+        gradient = QLinearGradient(w * 0.30, 0, w, 0)
+        gradient.setColorAt(0.0, QColor(255, 255, 255, 240))
+        gradient.setColorAt(0.45, QColor(255, 255, 255, 240))
+        gradient.setColorAt(1.0, QColor(255, 255, 255, 0))
+
+        painter.setPen(Qt.NoPen)
+        painter.setBrush(QBrush(gradient))
+        painter.drawRect(0, 0, w, h)
+
+        font = QFont("Microsoft YaHei", int(FONT_SIZE / _get_dpi_scale()))
+        painter.setFont(font)
+        painter.setPen(QColor(TEXT_COLOR))
+        fm = QFontMetrics(font)
+        tw = fm.horizontalAdvance(self._msg)
+        tx = int((w - tw) // 2)
+        ty = int((h + fm.ascent()) // 2 - 1)
+        painter.drawText(tx, ty, self._msg)
+
+        painter.end()
+
+
+_notices = []
+
+
+def notice(msg="", last=3):
+    screen = _get_app().primaryScreen()
+    sw = screen.size().width()
+    dpi_s = 1.0 / _get_dpi_scale()
+    w = int(sw * NOTICE_WIDTH_RATIO)
+    h = int(NOTICE_HEIGHT * dpi_s)
+    x = 0
+    offset = len(_notices) * (h + 4)
+    widget = _NoticeWidget(msg, last, x, offset, w)
+    _notices.append(widget)
+    widget.show()
+    widget.raise_()
+
+    loop = QEventLoop()
+    widget.destroyed.connect(loop.quit)
+    loop.exec()
+
+    _cleanup(widget)
+    return widget
+
+
+def _cleanup(obj):
+    try:
+        _notices.remove(obj)
+    except ValueError:
+        pass
---
+++ noticeboxTest.py
@@ -0,0 +1,3 @@
+import dokibox
+
+dokibox.notice("123")
